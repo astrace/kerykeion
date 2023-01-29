be1057db6050493bb3737e5192795eca1780233c
Sasa Milic <milichkam@gmail.com>
2023-01-29 18:52:45 +0100

    updated cache session initialization

--- kerykeion/fetch_geonames.py
+++ kerykeion/fetch_geonames.py
@@ -6,7 +6,7 @@
 
 import logging
 from requests import Request
-from requests_cache import CachedSession
+from requests_cache import CachedSession, SQLiteCache
 from typing import Union
 
 logging.basicConfig(
@@ -33,12 +33,8 @@ class FetchGeonames:
         username: str = "century.boy",
         logger: Union[logging.Logger, None] = None
     ):
-
-        self.session = CachedSession(
-            cache_name='cache/kerykeion_geonames_cache',
-            backend='sqlite',
-            expire_after=86400
-        )
+        backend = SQLiteCache(timeout=30, use_temp=True)
+        self.session = CachedSession(backend=backend)
 
         self.username = username
         self.city_name = city_name
